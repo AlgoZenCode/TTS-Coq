14097c0311ef554e0c806cf55954e0d1cab4a252
AlgoZenCode <divyalok6@gmail.com>
2025-09-02 21:29:51 +0530

    Create footer.jsx

---
+++ frontend/src/component/footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="mt-10 p-4 bg-gray-800 text-white text-center">
+      <p>© 2025 TTS Voice Generator | Powered by Coqui TTS</p>
+    </footer>
+  );
+}
