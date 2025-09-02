92d79bd092c8b09d68edce1d6d73fb08037f2b5f
AlgoZenCode <divyalok6@gmail.com>
2025-09-02 22:30:44 +0530

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,1 +1,14 @@
 
+import React from "react";
+import TTSPlayer from "./components/TTSPlayer";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>🎤 Coqui TTS Demo</h1>
+      <TTSPlayer />
+    </div>
+  );
+}
+
+export default App;
